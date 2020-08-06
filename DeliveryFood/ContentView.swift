6095f2fbc2d22451e2315adba27e636d9ad04266
Vladimir Tanev <vladimir.tanev.ios@gmail.com>
2020-08-06 22:38:12 +0200

    Delivery App
    Awsome experiment with XCode 12

---
+++ DeliveryFood/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  DeliveryFood
+//
+//  Created by Vladimir Tanev on 8/6/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            OnBoarding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DeliveryFood/DeliveryFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeliveryFoodApp.swift
+//  DeliveryFood
+//
+//  Created by Vladimir Tanev on 8/6/20.
+//
+
+import SwiftUI
+
+@main
+struct DeliveryFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
